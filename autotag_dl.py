44b94b3c195d0aff72aec97a8091c6379fe02c20
LucienToiture <74002516+LucienToiture@users.noreply.github.com>
2020-11-08 22:07:20 +0100

    Add files via upload
    Adds playlist downloading support and fixes missing temp folder issue.

--- autotag_dl.py
+++ autotag_dl.py
@@ -1,16 +1,11 @@
 from autotag_core import *
 import youtube_dl
+from youtube_dl.postprocessor.common import PostProcessor
 import pyperclip
 import time
 
 audio_format = 'mp3'
 audio_quality = '192'
-
-
-def get_output_filename(d):
-    if d['status'] == 'finished':
-        global output_filename
-        output_filename = d['filename']
 
 
 class MyLogger(object):
@@ -24,32 +19,10 @@ class MyLogger(object):
         print(msg)
 
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'outtmpl': '/Downloaded/%(title)s.%(ext)s',
-    'progress_hooks': [get_output_filename],
-    'logger': MyLogger(),
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': audio_format,
-        'preferredquality': audio_quality,
-    }]}
-
-ydl = youtube_dl.YoutubeDL(ydl_opts)
-pyperclip.copy("")
-clipboard = ""
-while clipboard != "exit":
-    clipboard = pyperclip.paste()
-    if 'youtube.com' in clipboard:
-        try:
-            print(f"Downloading {clipboard}")
-            ydl.download([clipboard])
-        except:
-            print("Downloading error. URL might be incorrect or not supported yet.")
-            pyperclip.copy("")
-            continue
+class AutotagPP(PostProcessor):
+    def run(self, information):
         print("Download complete, now fetching metadatas.")
-        output_filename = os.path.splitext(output_filename)[0] + "." + audio_format
+        output_filename = information['filepath']
 
         infos = GetInfo(output_filename)
         if infos[0] == 'null':
@@ -58,6 +31,34 @@ while clipboard != "exit":
         set_metadata(output_filename, infos)
 
         print(f"Downloaded {infos[0]} by {infos[1]}")
-        pyperclip.copy("")
+        print("\n")
+        return [], information
 
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': '/Downloaded/%(title)s.%(ext)s',
+    'logger': MyLogger(),
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': audio_format,
+        'preferredquality': audio_quality,
+    }]}
+
+ydl = youtube_dl.YoutubeDL(ydl_opts)
+ydl.add_post_processor(AutotagPP(None))
+pyperclip.copy("")
+clipboard = ""
+while clipboard != "exit":
+    clipboard = pyperclip.paste()
+    if 'youtube.com' in clipboard:
+        try:
+            print(f"Downloading {clipboard}")
+            print('\n')
+            ydl.download([clipboard])
+        except:
+            print("Downloading error. URL might be incorrect or not supported yet.")
+            pyperclip.copy("")
+            continue
+        pyperclip.copy("")
     time.sleep(1)
